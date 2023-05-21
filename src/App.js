bb01e3eee0aa255a95eba4adcd33c35ae7446d28
It's me Omario <omariokomarik@gmail.com>
2023-05-21 17:46:54 +0300

    Победа и поражение, изменяемый размер поля

--- src/App.js
+++ src/App.js
@@ -2,28 +2,73 @@ import './App.css';
 import { useState, useEffect } from 'react'
 
 function App() {
-  const [size, setSize] = useState(15)
+  const [size, setSize] = useState(20)
   const [field, setField] = useState([])
   const [game_over, setGame_over] = useState(false)
+  const [max_flags, setMax_flags] = useState(62)
+  const [flags, setFlags] = useState(0)
+  function change_size(event) {
+    let new_size = event.target.value
+    if (new_size < 5 || new_size > 50) {
+      alert("Ширина должна быть от 5 до 50 клеток")
+    } else {
+      setSize(new_size)
+    }
+  }
   function click(x, y) {
     if (game_over) {
       return
     }
     let new_field = field.slice()
-    for (let i = 0; i < size; i++) {
-      if (i == y) {
-        for (let j = 0; j < size; j++) {
-          if (j == x) {
-            if (new_field[i][j].bomb) {
-              for (let k = 0; k < size; k++) {
-                for (let l = 0; l < size; l++) {
-                  new_field[k][l].show = true
-                  setGame_over(true)
-                }
-              }
-            } else {
-              new_field[i][j].show = true
-            }
+    if (new_field[y][x].bomb) {
+      setGame_over(true)
+      alert("Вы проиграли")
+      for (let k = 0; k < size; k++) {
+        for (let l = 0; l < size; l++) {
+          new_field[k][l].show = true
+        }
+      }
+    } else {
+      new_field[y][x].show = true
+      if (new_field[y][x].bombs_around == "") {
+        if (y > 0 && x > 0) {
+          if (!new_field[y - 1][x - 1].show) {
+            click(x - 1, y - 1)
+          }
+        }
+        if (x < size - 1 && y < size - 1) {
+          if (!new_field[y + 1][x + 1].show) {
+            click(x + 1, y + 1)
+          }
+        }
+        if (x < size - 1 && y > 0) {
+          if (!new_field[y - 1][x + 1].show) {
+            click(x + 1, y - 1)
+          }
+        }
+        if (x > 0 && y < size - 1) {
+          if (!new_field[y + 1][x - 1].show) {
+            click(x - 1, y + 1)
+          }
+        }
+        if (x < size - 1) {
+          if (!new_field[y][x + 1].show) {
+            click(x + 1, y)
+          }
+        }
+        if (x > 0) {
+          if (!new_field[y][x - 1].show) {
+            click(x - 1, y)
+          }
+        }
+        if (y < size - 1) {
+          if (!new_field[y + 1][x].show) {
+            click(x, y + 1)
+          }
+        }
+        if (y > 0) {
+          if (!new_field[y - 1][x].show) {
+            click(x, y - 1)
           }
         }
       }
@@ -35,25 +80,22 @@ function App() {
       return
     }
     let new_field = field.slice()
-    for (let i = 0; i < size; i++) {
-      if (i == y) {
-        for (let j = 0; j < size; j++) {
-          if (j == x) {
-            if (new_field[i][j].flag) {
-              new_field[i][j].show = false
-              new_field[i][j].flag = false
-            } else {
-              new_field[i][j].show = true
-              new_field[i][j].flag = true
-            }
-          }
-        }
+    if (new_field[y][x].flag) {
+      new_field[y][x].show = false
+      new_field[y][x].flag = false
+      setFlags(flags - 1)
+    } else {
+      if (flags < max_flags) {
+        new_field[y][x].show = true
+        new_field[y][x].flag = true
+        setFlags(flags + 1)
       }
     }
     setField(new_field)
   }
   useEffect(() => {
-    let mines = Math.round(size * 2.5)
+    let mines = Math.floor(size * size * 10 / 64)
+    setMax_flags(mines)
     let trial_field = []
     let mines_positions = []
     let mine_position = []
@@ -152,21 +194,38 @@ function App() {
         }
       }
     }
+    setFlags(0)
+    setGame_over(false)
     setField(trial_field)
-  }, [])
+  }, [size])
+  if (field.length) {
+    let n = 0
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (field.length == size && field[i][j].show) {
+          n++
+        }
+      }
+    }
+    if (n == size * size) {
+      if (!game_over) {
+        alert("Вы победили")
+      }
+    }
+  }
   const Pole = field.map((row) => {
     return (row.map((cell) => {
       if (cell.flag) {
-        return (<div key={cell.X + '-' + cell.Y} onContextMenu={() => right_click(cell.X, cell.Y)} className={`Cell show_${cell.show}`}>
+        return (<div key={cell.X + '-' + cell.Y} style={{ height: 900 / size + "px" }} onContextMenu={() => right_click(cell.X, cell.Y)} className={`Cell show_${cell.show}`}>
           <img src='https://cdn3.iconfinder.com/data/icons/flatastic-1/256/flag-1024.png'></img>
         </div>)
       } else {
         if (cell.bomb) {
-          return (<div key={cell.X + '-' + cell.Y} onContextMenu={() => right_click(cell.X, cell.Y)} onClick={() => click(cell.X, cell.Y)} className={`Cell show_${cell.show}`}>
+          return (<div key={cell.X + '-' + cell.Y} style={{ height: 900 / size + "px" }} onContextMenu={() => right_click(cell.X, cell.Y)} onClick={() => click(cell.X, cell.Y)} className={`Cell show_${cell.show}`}>
             <img src='https://www.freeiconspng.com/uploads/bomb-transparent-background-4.png'></img>
           </div>)
         } else {
-          return (<div key={cell.X + '-' + cell.Y} onContextMenu={() => right_click(cell.X, cell.Y)} onClick={() => click(cell.X, cell.Y)} className={`Cell show_${cell.show}`}>
+          return (<div key={cell.X + '-' + cell.Y} style={{ height: 900 / size + "px" }} onContextMenu={() => right_click(cell.X, cell.Y)} onClick={() => click(cell.X, cell.Y)} className={`Cell show_${cell.show}`}>
             <p>{cell.bombs_around}</p>
           </div>)
         }
@@ -174,8 +233,11 @@ function App() {
     }))
   })
   return (
-    <div className="App">
-      {Pole}
+    <div>
+      <input type='number' onChange={change_size} placeholder='Ширина поля от 5 до 50' value={size}></input>
+      <div className="App">
+        {Pole}
+      </div>
     </div>
   );
 }
